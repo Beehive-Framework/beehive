e4ca2c14deb51bd71ddddd286f807718106efcba
jinhuajie <jinhuajie@tal.com>
2021-04-16 11:44:55 +0800

    update: 透传 options 参数给 lodash template 方法

--- build/vite/plugin/plugin-index-html-template.js
+++ build/vite/plugin/plugin-index-html-template.js
@@ -17,14 +17,20 @@ import { template } from 'lodash'
  * @param {Object} config 配置模版数据
  * @param {Object} config.data 静态模版数据
  * @param {Function} config.addData 动态添加模版数据，优先级更高
+ * @param {Object} config.options 透传给 lodash template 方法，参考文档 https://lodash.com/docs/4.17.15#template
  * @returns vite 插件实例
  */
 export const indexHtmlTemplate = config => {
-  const { data, addData = () => {} } = config || {}
+  const { data = {}, addData = () => {}, options = {} } = config || {}
   return {
     name: 'html-transform',
     transformIndexHtml(html, ctx) {
-      return template(html)({ indexHtmlTemplateData: Object.assign({}, data, addData(html, ctx)) })
+      const compiled = template(html, options)
+      return compiled({ indexHtmlTemplateData: Object.assign(
+        {},
+        data,
+        addData(html, ctx)
+      ) })
     }
   }
 }
