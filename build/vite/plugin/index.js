2374e630cb774d1bffa8124e4034828432b1c885
jinhuajie <jinhuajie@tal.com>
2021-04-19 18:12:33 +0800

    feat: 引入 inlineCode

--- build/vite/plugin/index.js
+++ build/vite/plugin/index.js
@@ -1,6 +1,7 @@
 import vue from '@vitejs/plugin-vue';
 import resolve from '@rollup/plugin-node-resolve'
 import indexHtmlTemplate from './plugin-index-html-template';
+import { initInlineCode } from './init-inline-code';
 import { indexJSReplace } from './plugin-index-js-replace';
 
 export function createVitePlugins (viteEnv, mode) {
@@ -11,6 +12,9 @@ export function createVitePlugins (viteEnv, mode) {
       extensions: [ '.mjs', '.js', '.jsx', '.json', '.vue' ],
     }),
     indexHtmlTemplate({
+      data: {
+        inlineCode: initInlineCode()
+      },
       addData(html, ctx) {
         return {
           cssBundles: Object.keys(ctx.bundle || {}).filter(b => /.*\.css$/.test(b)).map(b => `${viteEnv.VITE_PUBLIC_PATH}${b}`),
